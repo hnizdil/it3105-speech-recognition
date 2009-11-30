dd9e10900697350b0f80bc638ad1aa97e549bc4c
Robert Braunschweig <robert-braunschweig@gmx.de>
2009-11-30 18:56:34 +0100

    fixed classifier. but i think the program is still doing something wrong.

--- @classifier/classifier.m
+++ @classifier/classifier.m
@@ -5,7 +5,7 @@ classdef classifier
     
     methods
         function c = classifier
-            c.words = [hmm('start',5) , hmm('stop',4) , ...
+            c.words = [hmm('start',5) ,hmm('stop',4),  ...
                        hmm('left',4) , hmm('right',3)];
         end
         
@@ -13,7 +13,10 @@ classdef classifier
             maxP = 0;    %threshold for recognizing a word (?)
             hmmIndex = 0;
             for i = 1:size(obj.words,2) %go through all hmm and compare probability
-                P = forwardHMM(obj.words(i), data);
+                h = obj.words(i);
+                h.myWord;
+                [l f] = forwardHMM(h, data);
+                P = exp(l);
                 if(P > maxP)
                     maxP = P;
                     hmmIndex = i;
