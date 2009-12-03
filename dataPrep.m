55d05b2cd40167499db7b353a505825d0672935a
Jan Hnizdil <jan.hnizdil@dobryweb.cz>
2009-12-03 01:25:14 +0100

    finding peaks (not max values) in data preparation

--- dataPrep.m
+++ dataPrep.m
@@ -3,43 +3,35 @@
 %   Fs - sampling frequency/rate
 function [ prepData ] = dataPrep( data, Fs )
 
-L = Fs/100; %length of frame (1/100 of a sec)
-y = data;
+L = Fs / 100; %length of frame (1/100 of a sec)
 
 % power of 2 because of FFT algorithm
 NFFT = 2^nextpow2(L);
 
-%y=y.*hamming(length(y));
-
 % not sure about this normalization
-y = y ./ max(abs(y));
+data = data ./ max(abs(data));
 
 % overlapping is 50%
-frames = buffer(y, L, round(L/2), 'nodelay');
+frames = buffer(data, L, round(L/2), 'nodelay');
 
 dim = size(frames);
 
 % windowing and fft (F has same dimensions as frames)
 F = fft(frames .* repmat( hamming(dim(1)),1,dim(2) ), NFFT);
 
-% F is symmetric, so we can cut the lower half
+% F is symmetric, so we can cut the lower half off
 NFFT = NFFT / 2;
 
 F = abs(F(1:NFFT,:)) ./ (2*pi);
-
-% normalized version (for real values *Fs/2)
-freq = linspace(0, 1, NFFT);
 
 noFeatures = 5;
 
 % go through all frames
 for i=1:dim(2)
-	% capture greatest magnitude and according frequency
-	for j=1:noFeatures
-		[ amp x ] = max(F(:,i));
-		% get according frequency
-		prepData(j,:,i) = [ amp freq(x) ];
-		% remove last max value to find the second next round
-		F(x,i) = 0;
-	end
+    % capture greatest magnitude and according frequency
+    [ pks locs ] = findpeaks(F(:,i), 'sortstr', 'descend');
+    % amplitudes go to the first column ...
+    prepData(:,1,i) = pks(1:noFeatures);
+    % ... and frequencies (normalized to range 0-1) to the second one
+    prepData(:,2,i) = (locs(1:noFeatures)-1) / (NFFT-1);
 end
